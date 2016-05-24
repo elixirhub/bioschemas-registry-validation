170a0e8acea134837c701e9b3e9de92c7fcf2c52
Roberto Preste <roberto.preste@gmail.com>
2016-05-24 15:23:31 +0100

    Add function to compare scraped tags with Bioschemas ones

--- validation/validator.py
+++ validation/validator.py
@@ -6,6 +6,67 @@ websiteTypesSet = set()     # Set with all the Types (reported just once)
 websiteTags = {}            # Dict with all the Properties and the number of their occurrences
 websiteProps = {}           # Dict with all the Properties and their related data
 
+referenceProps = {}         # Dict with all the Properties found in the Bioschemas website
+
+sharedProps = {}            # Dict with the Properties found both in Bioschemas and in the scraped website
+
+def getReferenceTags():
+    """Get the latest Bioschemas tags."""
+
+    kinds = ["event", "person", "organization", "training"]
+
+    for kind in kinds:
+        base_url = "http://bioschemas.org/groups"
+        end_url = "%ss/full_%s.html" % (kind, kind)
+        url = "%s/%s" % (base_url, end_url)
+
+        response = requests.get(url)
+        html = response.content
+        soup = BeautifulSoup(html, "lxml")
+
+        bioschemas_table = soup.find("table", attrs={"class": "bioschemas"})
+        schema_table = soup.find("table", attrs={"class": "schema"})
+        thing_table = soup.find("table", attrs={"class": "thing"})
+
+        td_list = []
+        type_list = []
+        for row in bioschemas_table.findAll("tr"):
+            row_list = []
+            for cell in row.findAll("td"):
+                row_list.append(cell.text.encode("utf-8"))
+            td_list.append(row_list)
+        for i in xrange(1, len(td_list)):
+            prop_dict = {}
+            prop_dict[td_list[i].pop(0)] = td_list[i]
+            type_list.append(prop_dict)
+        referenceProps["%s_bioschemas" % kind] = type_list
+
+        td_list = []
+        type_list = []
+        for row in schema_table.findAll('tr'):
+            row_list = []
+            for cell in row.findAll('td'):
+                row_list.append(cell.text.encode("utf-8"))
+            td_list.append(row_list)
+        for i in xrange(1, len(td_list)):
+            prop_dict = {}
+            prop_dict[td_list[i].pop(0)] = td_list[i]
+            type_list.append(prop_dict)
+        referenceProps["%s_schema" % kind] = type_list
+
+        td_list = []
+        type_list = []
+        for row in thing_table.findAll('tr'):
+            row_list = []
+            for cell in row.findAll('td'):
+                row_list.append(cell.text.encode("utf-8"))
+            td_list.append(row_list)
+        for i in xrange(1, len(td_list)):
+            prop_dict = {}
+            prop_dict[td_list[i].pop(0)] = td_list[i]
+            type_list.append(prop_dict)
+        referenceProps["%s_schema" % kind] += type_list
+
 def validateMicrodata(url):
     """Validate the microdata content of a website."""
 
@@ -41,6 +102,18 @@ def validateMicrodata(url):
         else:
             websiteProps[prop] = [cont]
 
+    # Compare each Property with the ones found in the Bioschemas website
+    for prop in websiteTags:
+        for tag in referenceProps:
+            for el in referenceProps[tag]:
+                for key in el:
+                    if prop == key:
+                        try:
+                            sharedProps[tag] += [(prop, websiteTags[prop])]
+                        except KeyError:
+                            sharedProps[tag] = [(prop, websiteTags[prop])]
+
+    print sharedProps
 
 def validateRDFa(url):
     """Validate the RDFa content of a website."""
@@ -95,8 +168,9 @@ def validateJSONLD(url):
     print soup.findAll('script', type='application/ld+json')
 
 
+getReferenceTags()
 
-
+validateMicrodata("http://d.lib.ncsu.edu/collections/catalog/mc00383-001-ff0004-001-001_0004")
 #validateMicrodata("http://doulas.club/carla-ferro")
 #validateRDFa("http://www.booking.com/hotel/ru/radisson-slavyanskaya-business-center.en-gb.html")
 #validateJSONLD("https://github.com/mcollina/levelgraph")
