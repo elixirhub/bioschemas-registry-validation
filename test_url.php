1b5617eabb1699a35bdab1e0ae838386d1303332
Roberto Preste <roberto.preste@gmail.com>
2016-09-01 12:07:43 +0100

    Add mail notification function

--- test_url.php
+++ test_url.php
@@ -4,6 +4,7 @@ $url = $_GET['url'];
 $name = $_GET['nome'];
 $link = $_GET['link'];
 $types = $_GET['types'];
-exec("python tester.py $url $name $link $types");
+$mail = $_GET['mail'];
+exec("python tester.py $url $name $link $types $mail");
 
 ?>
--- validate_url.php
+++ validate_url.php
@@ -4,6 +4,7 @@ $url = $_GET['url'];
 $name = $_GET['nome'];
 $link = $_GET['link'];
 $types = $_GET['types'];
-exec("python validator.py $url $name $link $types");
+$mail = $_GET['mail'];
+exec("python validator.py $url $name $link $types $mail");
 
 ?>
